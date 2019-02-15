0634fdc739095e98e9de40fff68e63b6e1645a24
bencesomogyi <bence@bencesomogyi.com>
2019-02-15 13:31:26 +0100

    Release at Fri Feb 15 13:31:26 CET 2019

--- precache-manifest.4ce0017a7fe18aa4ec9347c6bedbac4f.js
+++ precache-manifest.4ce0017a7fe18aa4ec9347c6bedbac4f.js
@@ -20,6 +20,10 @@ self.__precacheManifest = [
     "url": "/android-chrome-192x192.png"
   },
   {
+    "revision": "c8972a038dda48010e18a55837b81d3e",
+    "url": "/apple-touch-icon.png"
+  },
+  {
     "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
     "url": "/browserconfig.xml"
   },
@@ -28,24 +32,20 @@ self.__precacheManifest = [
     "url": "/favicon-16x16.png"
   },
   {
-    "revision": "3088499e62dbb9374980877ff981b13e",
-    "url": "/favicon-32x32.png"
-  },
-  {
-    "revision": "c8972a038dda48010e18a55837b81d3e",
-    "url": "/apple-touch-icon.png"
-  },
-  {
-    "revision": "ff4e1dbcd5a3eddedec5578027178090",
-    "url": "/logo.svg"
-  },
-  {
     "revision": "9e4d1d81950d1dfd0af4de9fd50d1d05",
     "url": "/logo-grayscale.svg"
   },
   {
+    "revision": "3088499e62dbb9374980877ff981b13e",
+    "url": "/favicon-32x32.png"
+  },
+  {
     "revision": "4dc0634f1ce1f57627321f178e15d74a",
     "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "ff4e1dbcd5a3eddedec5578027178090",
+    "url": "/logo.svg"
   },
   {
     "revision": "92bd5f0b31d0f2470a2844e7c9f85ad4",
@@ -60,12 +60,12 @@ self.__precacheManifest = [
     "url": "/favicon.ico"
   },
   {
-    "revision": "dce6143516539c91bcf57b4030b80c5f",
-    "url": "/pepper.svg"
-  },
-  {
     "revision": "460fa0d08cb8c6fb87407f95b37babe6",
     "url": "/og-share.png"
+  },
+  {
+    "revision": "dce6143516539c91bcf57b4030b80c5f",
+    "url": "/pepper.svg"
   },
   {
     "revision": "ec79956c040ac87c19b711517134c10d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d65c8657cbe9856357c00ca89ddb073.js"
+  "/precache-manifest.4ce0017a7fe18aa4ec9347c6bedbac4f.js"
 );
 
 /**
